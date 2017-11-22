8e3dd7965a617acd6ef6cd1d3d0c8389ccd25171
zhouwt7 <zhouwt7@163.com>
2017-11-22 22:48:37 +0800

    完成求超过半数工人回答正确问题的功能

--- src/academic/Main.java
+++ src/academic/Main.java
@@ -6,27 +6,31 @@ package academic;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 5;
+        /**
+         * n 工人的数量
+         * data 工人质量参数
+         */
+        int n = 3;
 
-        double data[] = new double[]{1.0, 0.6, 0.7, 0.7, 0.8, 0.8};
+        double data[] = new double[]{1.0, 0.6, 0.7, 0.8};
 
-        double test[][] = new double[(n + 1) / 2][n];
+        double test[][] = new double[(n + 1) / 2 + 1][n + 1];
 
+        //根据质量推导公式，test[i][j]表示工人数量为j时，超过i个人回答正确的概率
         for (int i = 0; i <= (n + 1) / 2; i++) {
             for (int j = 0; j <= n; j++) {
-                if (i == 0 ||j == 0) {
+                if (i == 0) {
                     test[i][j] = 1;
                 } else if (i > j){
                     test[i][j] = 0;
+                }else if (j == 0){
+                    test[i][j] = 0;
                 }else {
                     test[i][j] = test[i - 1][j - 1] * data[j] + test[i][j - 1] * (1 - data[j]);
-                    System.out.println(test[i][j] + " " + i + " " + j);
-                    System.out.println();
                 }
-
             }
-
         }
+        System.out.println(test[(n + 1) / 2][n]);
 
 
 
